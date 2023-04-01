12e2590df94587a550dcf8c2fbef8f489c0ae9c3
theCrius <claudio.vallesi@gmail.com>
2023-04-01 17:46:14 +0100

    basic plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,12 @@
+# name: discourse-category-mod-enhancer
+# about: Discourse Category Mod Enhancer
+# version: 1.0
+# authors: Crius
+# url: https://github.com/netgamers-forum/discourse-category-mod-enhancer
+
+# Add CSS style
+register_asset "stylesheets/category-mod-enhancer.scss"
+
+after_initialize do
+    
+end
